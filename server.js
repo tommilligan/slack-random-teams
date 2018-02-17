32f1e0cb36d4636ed017755787ba4cb4f955ccca
Tom Milligan <code@tommilligan.net>
2018-02-17 11:37:47 +0000

    Working random-teams command

--- server.js
+++ server.js
@@ -1,43 +1,29 @@
-// server.js
+const express    = require('express');
+const bodyParser = require('body-parser');
 
-// BASE SETUP
-// =============================================================================
+let app = express();
+const port = process.env.PORT || 3000;
 
-// call the packages we need
-var express    = require('express');        // call express
-var app        = express();                 // define our app using express
-var bodyParser = require('body-parser');
+// for parsing application/json
+app.use(bodyParser.json());
+// for parsing application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
 
-var port = process.env.PORT || 3000;        // set our port
-
-app.use(bodyParser.json()); // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-
-// ROUTES FOR OUR API
-// =============================================================================
-var router = express.Router();              // get an instance of the express Router
-
-// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.get('/', function(req, res) {
-    console.log(req.body);
-    res.json({ message: 'hooray! welcome to our api!' });   
+// Get a router for our endpoints
+let router = express.Router();
+router.get('/health', function(req, res) {
+    res.sendStatus(200);
 });
-
-router.post('/', function(req, res) {
-    console.log(req.body);
-    res.json({ response: req.data });   
-});
-
 
 router.post('/random-teams', function(req, res) {
     const payload = req.body;
     console.log(payload);
 
-    const title = "*Your teams are:*";
+    const title = "_Your teams are:_";
 
     const teamNames = payload.text.split(' ');
     const teamParagraphs = teamNames.map(teamName => {
-        return `_${teamName}_`
+        return `*${teamName}*`
     })
 
     const paragraphs = [].concat([title], teamParagraphs)
