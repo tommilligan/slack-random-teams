9c9e80ff566ea868c7b6178bc59da6b00a52affb
Tom Milligan <code@tommilligan.net>
2018-02-18 20:55:37 +0000

    Basics of slack oauth added

--- src/config.js
+++ src/config.js
@@ -1,5 +1,7 @@
 import bodyParser from 'body-parser';
 import morgan from 'morgan';
+import passport from 'passport';
+const SlackStrategy = require('@aoberoi/passport-slack').default.Strategy;
 
 const applyConfig = app => {
   // for parsing application/json
@@ -8,6 +10,16 @@ const applyConfig = app => {
   app.use(bodyParser.urlencoded({ extended: true }));
   // logging
   app.use(morgan('combined'));
+
+  // Authorization
+  passport.use(new SlackStrategy({
+    clientID: process.env.SLACK_CLIENT_ID,
+    clientSecret: process.env.SLACK_CLIENT_SECRET,
+  }, (accessToken, scopes, team, extra, profiles, done) => {
+    done(null, profiles.user);
+  }));
+  app.use(passport.initialize());
+
   return app;
 };
 
