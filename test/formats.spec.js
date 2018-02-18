f0c62e82c39a6c3f001ea20042c84f5f2b518fc5
Tom Milligan <code@tommilligan.net>
2018-02-18 18:51:29 +0000

    Altered package json scripts

--- test/formats.spec.js
+++ test/formats.spec.js
@@ -6,7 +6,7 @@ describe('formatTeams', function() {
   it('should output format correctly', function() {
     var input = [['foo', ['a', 'b']], ['bar', ['c', 'd']]];
     var actual = subject.formatTeams(input);
-    expect(actual).to.equal(`Your teams are:_
+    expect(actual).to.equal(`_Your teams are:_
 *foo*
 a
 b
