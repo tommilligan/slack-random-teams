926c5690a49e6fa82f0c7baa6c7e99dc4fd1df21
Tom Milligan <code@tommilligan.net>
2018-02-18 20:37:59 +0000

    Added morgan logging

--- src/config.js
+++ src/config.js
@@ -1,10 +1,13 @@
 import bodyParser from 'body-parser';
+import morgan from 'morgan';
 
 const applyConfig = app => {
   // for parsing application/json
   app.use(bodyParser.json());
   // for parsing application/x-www-form-urlencoded
   app.use(bodyParser.urlencoded({ extended: true }));
+  // logging
+  app.use(morgan('combined'));
   return app;
 };
 
