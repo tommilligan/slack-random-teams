1749f678f143c96cd99031860820a5b0f37c1c77
Tom Milligan <code@tommilligan.net>
2018-02-24 15:35:29 +0000

    Replaced collection shuffler

--- src/utils.js
+++ src/utils.js
@@ -1,14 +1,4 @@
-/**
- * Shuffles array in place. ES6 version
- * @param {Array} a items An array containing the items.
- */
-export function shuffle(a) {
-  for (let i = a.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [a[i], a[j]] = [a[j], a[i]];
-  }
-  return a;
-}
+import shuffle from 'shuffle-array';
 
 /**
  * Splits and array into equalish length chunks
--- test/utils.spec.js
+++ test/utils.spec.js
@@ -2,14 +2,6 @@ var expect = require('chai').expect;
 
 var subject = require('../src/utils');
 
-describe('shuffle', function() {
-  it('should contain same elements', function() {
-    var input = [0, 1, 2, 3];
-    var actual = subject.shuffle(input);
-    expect(actual).to.have.all.members(input);
-  });
-});
-
 describe('chunkArray', function() {
   it('should distribute elements', function() {
     var input = [0, 1, 2, 3];
